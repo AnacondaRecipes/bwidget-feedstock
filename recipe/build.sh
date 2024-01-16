70e08bbeb0744e7eed416c546653175a107ba06a
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2024-01-16 12:30:10 +0200

    Update recipe/build.sh
    Co-authored-by: Ian Fitchet <123584373+ifitchet@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ TCL_VERSION="$(echo 'puts [info tclversion]' | tclsh)"
 TCL_LIB_PATH="$PREFIX"/lib/tcl"$TCL_VERSION"
 BWIDGET_PATH="$TCL_LIB_PATH"/bwidget
 
-[[ ! -d "$BWIDGET_PATH" ]] || exit 1
+[[ -d "$BWIDGET_PATH" ]] && exit 1
 mkdir -p "$BWIDGET_PATH"
 
 # conda-build requires the license file for the packaging stage so we need it in the src folder
