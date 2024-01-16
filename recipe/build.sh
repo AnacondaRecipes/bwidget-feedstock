773117751926cf4cd5f88cb839b0f28dfd78af22
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-16 10:07:01 +0200

    Do not move the license file, only copy it

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,4 +10,6 @@ BWIDGET_PATH="$TCL_LIB_PATH"/bwidget
 
 [[ ! -d "$BWIDGET_PATH" ]] || exit 1
 mkdir -p "$BWIDGET_PATH"
-mv * "$BWIDGET_PATH"/
+# conda-build requires the license file for the packaging stage so we need it in the src folder
+mv *(!src/LICENSE.txt) "$BWIDGET_PATH"/
+cp src/LICENSE.txt "$BWIDGET_PATH"/
